83d97ed5626ba2bf936a3b997909a4e18bea97c6
yedio <yediobjet@gmail.com>
2023-02-14 12:29:45 +0900

    setting: 폴더구조

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        display: 'flex',
+        padding: '0 15px',
+        alignItems: 'center',
+        width: '100vw',
+        height: '60px',
+        fontSize: '30px',
+        backgroundColor: 'navy',
+      }}
+    >
+      ❄️
+    </header>
+  );
+}
